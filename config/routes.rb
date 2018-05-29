4efa571455269f1052edd00695a7008848f6f94a
Albeiro <albeiroespitiasierra@gmail.com>
2018-05-29 02:26:23 -0500

    Agrego vistas de otros departamentos pqrtrackings

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   resources :roles
   resources :permissions
   root to: "home#index"
+  get '/pqr_trackingsDepartment', to: 'pqr_trackings#departmentView'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
