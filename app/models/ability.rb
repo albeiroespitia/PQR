d5d4be80ffcb6717d15d10d2f2e91d56d6e07135
Albeiro <albeiroespitiasierra@gmail.com>
2018-05-29 03:06:20 -0500

    Completo permisos con cancan gem

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,19 +2,156 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    alias_action :create, :read, :update, :destroy, to: :crud
     # Define abilities for the passed in user here. For example:
     if !user.nil?
       if user.role.name == 'Director'
+        #PQR
         can :create, Pqr
+        cannot :update, Pqr
+        cannot :destroy, Pqr
+        can :read, Pqr
+        #DEPARTMENT
+        cannot :create, Department
+        cannot :update, Department
+        cannot :destroy, Department
+        can :read, Department
+        #EXTERNAL_FILES
+        cannot :create, ExternalFile
+        cannot :update, ExternalFile
+        cannot :destroy, ExternalFile
+        can :read, ExternalFile
+        #PQRTRACKINGS
+        can :create, PqrTracking
+        cannot :update, PqrTracking
+        cannot :destroy, PqrTracking
+        can :read, PqrTracking
+        #ROLES
+        cannot :create, Role
+        cannot :update, Role
+        cannot :destroy, Role
+        can :read, Role
+        #STATUSES
+        cannot :create, Status
+        cannot :update, Status
+        cannot :destroy, Status
+        can :read, Status
+        #USERS
+        cannot :create, User
+        can :update, User
+        can :destroy, User
+        can :read, User
       elsif user.role.name == 'Admin'
         can :manage, :all
       elsif user.role.name == 'Esclavo'
-        #
+        #PQR
+        can :create, Pqr
+        cannot :update, Pqr
+        cannot :destroy, Pqr
+        cannot :read, Pqr
+        #DEPARTMENT
+        cannot :create, Department
+        cannot :update, Department
+        cannot :destroy, Department
+        cannot :read, Department
+        #EXTERNAL_FILES
+        cannot :create, ExternalFile
+        cannot :update, ExternalFile
+        cannot :destroy, ExternalFile
+        cannot :read, ExternalFile
+        #PQRTRACKINGS
+        can :create, PqrTracking
+        cannot :update, PqrTracking
+        cannot :destroy, PqrTracking
+        cannot :read, PqrTracking
+        #ROLES
+        cannot :create, Role
+        cannot :update, Role
+        cannot :destroy, Role
+        cannot :read, Role
+        #STATUSES
+        cannot :create, Status
+        cannot :update, Status
+        cannot :destroy, Status
+        cannot :read, Status
+        #USERS
+        cannot :create, User
+        can :update, User
+        can :destroy, User
+        can :read, User
       elsif user.role.name == 'Lider'
-        # 
+        #PQR
+        can :create, Pqr
+        cannot :update, Pqr
+        cannot :destroy, Pqr
+        can :read, Pqr
+        #DEPARTMENT
+        cannot :create, Department
+        cannot :update, Department
+        cannot :destroy, Department
+        can :read, Department
+        #EXTERNAL_FILES
+        can :create, ExternalFile
+        cannot :update, ExternalFile
+        cannot :destroy, ExternalFile
+        can :read, ExternalFile
+        #PQRTRACKINGS
+        can :create, PqrTracking
+        cannot :update, PqrTracking
+        cannot :destroy, PqrTracking
+        can :read, PqrTracking
+        #ROLES
+        cannot :create, Role
+        cannot :update, Role
+        cannot :destroy, Role
+        can :read, Role
+        #STATUSES
+        cannot :create, Status
+        cannot :update, Status
+        cannot :destroy, Status
+        can :read, Status
+        #USERS
+        cannot :create, User
+        can :update, User
+        can :destroy, User
+        can :read, User
       end
     else
-      can :create, Pqr
+        #PQR
+        can :create, Pqr
+        cannot :update, Pqr
+        cannot :destroy, Pqr
+        cannot :read, Pqr
+        #DEPARTMENT
+        cannot :create, Department
+        cannot :update, Department
+        cannot :destroy, Department
+        cannot :read, Department
+        #EXTERNAL_FILES
+        cannot :create, ExternalFile
+        cannot :update, ExternalFile
+        cannot :destroy, ExternalFile
+        cannot :read, ExternalFile
+        #PQRTRACKINGS
+        cannot :create, PqrTracking
+        cannot :update, PqrTracking
+        cannot :destroy, PqrTracking
+        can :read, PqrTracking
+        #ROLES
+        cannot :create, Role
+        cannot :update, Role
+        cannot :destroy, Role
+        cannot :read, Role
+        #STATUSES
+        cannot :create, Status
+        cannot :update, Status
+        cannot :destroy, Status
+        cannot :read, Status
+        #USERS
+        can :create, User
+        cannot :update, User
+        cannot :destroy, User
+        cannot :read, User
     end
 
 
